3426f012f19322ba0d8cd346e5b2246832a8d326
SrPabvliss <pablomartinvillacres@gmail.com>
2025-07-08 22:36:52 -0500

    fix: cd updated

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,13 @@ import { BrowserRouter as Router } from 'react-router'
 import { ThemeProvider } from '@/core/providers/theme-provider'
 import { AppRoutes } from './routes/routes'
 
+const basename =
+  import.meta.env.MODE === 'production' ? '/senirop-cms-demo' : '/'
+
 function App() {
   return (
     <ThemeProvider>
-      <Router>
+      <Router basename={basename}>
         <AppRoutes />
       </Router>
     </ThemeProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/senirop-cms-demo/' : '/',
+  base: mode === 'production' ? '/senirop-cms-demo/' : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
       '@mock': resolve(__dirname, './src/core/mock'),
     },
   },
-})
+}))
